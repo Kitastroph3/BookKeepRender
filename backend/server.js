9028f37d93d99be95c4aa5d552beb6d61fc13d56
Kit Danovsky <cadanovsky@gmail.com>
2024-02-22 11:50:31 -0500

    cors

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,9 @@ const port = process.env.PORT || 5000;
 connectDB();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "https://book-keep-render.vercel.app/"
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
