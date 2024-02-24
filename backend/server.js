5579cb8e2ab5271a3a231ca5d83f428ce4e69906
Kit Danovsky <cadanovsky@gmail.com>
2024-02-24 18:53:53 -0500

    cors

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,9 @@ const port = process.env.PORT || 5000;
 connectDB();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    methods: [ 'GET', 'POST', 'PUT', 'DELETE' ]
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
