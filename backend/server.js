e30078695b23bd2bdb6c5b2ec78edd4574d1584c
Kit Danovsky <cadanovsky@gmail.com>
2024-02-24 21:39:52 -0500

    docker

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const { errorHandler } = require('./middleware/errorMiddleware')
 const dotenv = require('dotenv').config();
 const connectDB = require('./connect/database');
-const port = process.env.PORT || 3005;
+const port = process.env.PORT || 8000;
 
 connectDB();
 const app = express();
